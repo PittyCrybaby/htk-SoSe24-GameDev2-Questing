50788dddff952a85d69733e649ef9858463d0602
PittyCrybaby <tanjalange@hotmail.de>
2024-04-12 17:52:20 +0200

    Dialog Button

--- Assets/Scripts/StoryView.cs
+++ Assets/Scripts/StoryView.cs
@@ -16,7 +16,7 @@ public class StoryView : MonoBehaviour
     [SerializeField] private TextMeshProUGUI storyText;
     [SerializeField] private TextMeshProUGUI speakerName;
     [SerializeField] private Button buttonPrefab;
-    [SerializeField] private QuestsConfig questConfig;
+    // TODO add later [SerializeField] private QuestsConfig questConfig;
 
     private void Awake()
     {
@@ -77,6 +77,7 @@ public class StoryView : MonoBehaviour
         }
     }
 
+    /* TODO: add later
     private void HandleTags()
     {
         if (story.currentTags.Count <= 0)
@@ -104,6 +105,7 @@ public class StoryView : MonoBehaviour
             }
         }
     }
+    */
 
     private void OnClickChoiceButton(Choice choice)
     {
@@ -128,7 +130,7 @@ public class StoryView : MonoBehaviour
 
     private void DestroyOldChoices()
     {
-        foreach (Transform child in transform)
+        foreach (Transform child in choiceHolder)
         {
             Destroy(child.gameObject);
         }
