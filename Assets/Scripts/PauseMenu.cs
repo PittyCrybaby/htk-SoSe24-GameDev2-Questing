1925914e32e3661178e15090fbec09b863aecc82
PittyCrybaby <tanjalange@hotmail.de>
2024-04-19 18:00:00 +0200

    PausePanel

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.UI;
+
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject PausePanel;
+        [SerializeField] private Button ContinueButton;
+
+        private void Awake()
+        {
+            PausePanel.SetActive(false);
+            ContinueButton.onClick.AddListener(() => SetPausedStatus(false));
+        }
+
+        private void OnMouseUpAsButton()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                var wasPreviouslyPaused = PausePanel.activeSelf;
+                SetPausedStatus(!wasPreviouslyPaused);
+            }
+        }
+        
+        private void SetPausedStatus(bool isPaused)
+        {
+            PausePanel.SetActive(isPaused);
+            Time.timeScale = isPaused ? 0 : 1;
+            Cursor.visible = isPaused;
+            Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
