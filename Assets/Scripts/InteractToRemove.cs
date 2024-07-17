4fa6ca5e4c60eacc0a6a5983b1ea642858d2af61
PittyCrybaby <143957326+PittyCrybaby@users.noreply.github.com>
2024-07-17 10:09:59 +0200

    WaterShader

---
+++ Assets/Scripts/InteractToRemove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractToRemove : MonoBehaviour, IInteractable
+{
+    [Serializefield] private GameObject _deleteThisGameObject;
+    
+    
+    
+}
